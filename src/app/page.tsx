c5b93c42850105476b5d9a4ee74bab256cfa0f0c
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-18 18:59:50 +0200

    ffeat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-2xl font-bold">Farm2Table</p>
+    </div>
+  );
+}
