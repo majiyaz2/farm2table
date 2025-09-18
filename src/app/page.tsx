b6bb6e386e1861ef87e7a0c576eb6b39dbf35622
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-18 19:43:25 +0200

    feat: cutomized  ui elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
-
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-4">
       <p className="text-2xl font-bold">Farm2Table</p>
+      <Button variant="elevated">Button</Button>
+      <Input placeholder="Enter your name" />
+      <Progress value={50} />
+      <Textarea placeholder="Enter your message" />
+      <Checkbox />
     </div>
   );
 }
+ 
