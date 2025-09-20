14764145d096293a8acef1090ff9c632173829c3
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-20 11:17:18 +0200

    feat: added a searchfilter page

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,34 @@
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
+import { SearchFilters } from "./search-filters";
+
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+    const payload = await getPayload({
+        config: configPromise,
+    });
+
+    const data = await payload.find({
+        collection: "categories",
+        where: {
+            parent: {
+                exists: false,
+            },
+        },
+        depth: 1,
+    });
+    
     return (
         <div className="flex flex-col min-h-screen">
            <Navbar />
+           <SearchFilters data={data}/>
            <div className="flex-1 bg-[#F4F4F0]">
                 {children}
            </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,24 +1,8 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
-
 export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-
-  const data = await payload.find({
-    collection: "categories",
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-     depth: 1,
-  });
-
+ 
   return (
    <div>
-    {JSON.stringify(data)}
+     Home Page
    </div>
   );
 }
