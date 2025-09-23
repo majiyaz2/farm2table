2e2f28af7ebeb0efded2f5988fac924f422e0663
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-23 15:50:00 +0200

    feat: added the categories procedure

--- src/modules/categories/server/procedures.ts
+++ src/modules/categories/server/procedures.ts
@@ -1,7 +1,30 @@
+
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Category } from "@/payload-types";
 
 export const categoriesRouter = createTRPCRouter({
-    getMany: baseProcedure.query(() => {
-      return [{hello: "world"}]  
+    getMany: baseProcedure.query(async ({ctx}) => {
+      
+          const data = await ctx.db.find({
+              collection: "categories",
+              where: {
+                  parent: {
+                      exists: false,
+                  },
+              },
+              depth: 1,
+              pagination: false,
+              sort: "name",
+          });
+      
+          const formattedData = data.docs.map((doc) => ({
+              ...doc,
+              subcategories: (doc.subcategories?.docs ?? []).map((sub) => ({
+                  ...(sub as Category),
+                  subcategories: undefined,
+              }))
+          }));
+
+          return formattedData;
     })
 })
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,5 +1,7 @@
 import { initTRPC } from '@trpc/server';
 import { cache } from 'react';
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -19,4 +21,9 @@ const t = initTRPC.create({
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
-export const baseProcedure = t.procedure;
+export const baseProcedure = t.procedure.use(async ({next }) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  return next({ctx: {db: payload}});
+});
