9ba908f1d1384161ca404c51f13fc54c4207bee3
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-23 13:52:15 +0200

    feat: added a trpc server

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,18 @@
-export default async function Home() {
+"use client"
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+export default function Home() {
+
  
+  const trpcClient = useTRPC();
+  const categories = useQuery(trpcClient.categories.getMany.queryOptions())
   return (
+
+
    <div>
-     Home Page
+    {categories.isLoading && <p>Loading...</p>}
+    {categories.error && <p>Error: {categories.error.message}</p>}
+     {JSON.stringify(categories.data, null, 2)}
    </div>
   );
 }
