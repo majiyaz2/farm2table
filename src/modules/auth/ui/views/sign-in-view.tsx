6b5fa6658603c1745a2edadf68f286b7f652a152
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-24 14:31:07 +0200

    feat: added a view for sign up and sign in

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,16 @@
+export const SignInView = () => {
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-5">
+            <div className="bg-[#f4f4f0] h-screen w-full lg:col-span-3 overflow-y-auto">
+                Form column
+            </div>
+            <div className="h-screen w-full lg:col-span-2 hidden lg:block"
+                style={{
+                    backgroundImage: "url('/auth-bg.png')",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            />
+        </div>
+    )
+}
