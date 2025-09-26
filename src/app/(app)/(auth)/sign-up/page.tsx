ece8022d991592e3f045061c564093586b3e41cc
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-26 14:34:38 +0200

    feat: fixed redirects when user is logged in

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -1,6 +1,12 @@
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
+import { redirect } from "next/navigation"
+import { caller } from "@/trpc/server"
 
-const Page = () => {
+const Page = async () => {
+    const session = await caller.auth.session()
+    if(session.user){
+        redirect("/")
+    }
     return (
         <SignUpView/>
     )
