e58bef58402d4733a28bc4bdb46d285166110908
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-09-29 16:01:24 +0200

    chore: added the ability to filter results based on subcategory

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,17 +1,29 @@
+import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
     params: Promise<{
-        category: string
         subcategory: string
     }>;
 }
 
 const CategoryPage = async ({params}: Props) => {
-    const {category, subcategory} = await params;
+    const queryClient = getQueryClient();
+    const {subcategory} = await params;
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category: subcategory,
+    }))
+    
+
+    
     return (
-        <div>
-            <h1>Category Page: {category}</h1>
-            <h2>Subcategory Page: {subcategory}</h2>
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <ProductList category={subcategory} />
+            </Suspense>
+        </HydrationBoundary>
     );
 };
 
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,24 +1,53 @@
 
+import { Category } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { Where } from "payload";
 import { z } from "zod";
-
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure.input(z.object({
         category: z.string().nullable().optional(),
     })).query(async ({ctx, input}) => {
+        const where:Where = {};
 
-       const where:Where = {};
+        if (input.category) {
+         const categoriesData = await ctx.db.find({
+            collection: "categories",
+            pagination: false,
+            limit: 1,
+            depth: 1,
+            where: {
+                slug: {
+                    equals: input.category,
+                }
+            },
+         });   
+
+           const formattedData = categoriesData.docs.map((doc) => ({
+                ...doc,
+                subcategories: (doc.subcategories?.docs ?? []).map((sub) => ({
+                    ...(sub as Category),
+                    subcategories: [],
+                }))
+            }));
+         const subcategorySlugs:string[] = []
+         const parentCategory = formattedData[0];
+
+         if (parentCategory) {
+            subcategorySlugs.push(
+                ...parentCategory.subcategories.map((subcategory) => subcategory.slug)
+            )       
+        }
+        where["category.slug"] = {
+            in: [parentCategory.slug, ...subcategorySlugs]
+        };
+        }
+
       
-       where["category.slug"] = {
-        equals: input.category
-    };
-
         const data = await ctx.db.find({
             collection: "products",
             depth: 1,
-            where
+            where,
         });
 
        
