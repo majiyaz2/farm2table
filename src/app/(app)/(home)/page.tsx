bb767840d1e28810dca371c212307818e0881a31
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-10-07 11:34:03 +0200

    feat: added view all products funtionality

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,20 +1,31 @@
-"use client"
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import type { SearchParams } from "nuqs/server";
+import { loadProductFilters } from "@/modules/products/searchParams";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_PAGE_LIMIT } from "@/constants";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
-export default function Home() {
-  const trpc = useTRPC()
-
-  const {data} = useQuery(trpc.auth.session.queryOptions())
-
-  return (
-
-
-   <div>
-      {JSON.stringify(data?.user, null , 2)}
-   </div>
-  );
+interface Props {
+    searchParams: Promise<SearchParams>
 }
- 
+
+const CategoryPage = async ({searchParams}: Props) => {
+    const filters  =  loadProductFilters(searchParams);
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
+   
+        ...filters,
+        limit: DEFAULT_PAGE_LIMIT,
+    }))
+    
+
+    
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ProductListView />
+        </HydrationBoundary>
+    );
+};
+
+export default CategoryPage;
