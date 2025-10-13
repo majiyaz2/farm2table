80f888e4b52c02b9d4e603398b2c176b44b4c829
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-10-14 00:20:47 +0200

    feat: implemented checkout UI for product items

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -5,12 +5,14 @@ import { useCart } from "../../hooks/use-cart";
 import { useTRPC } from "@/trpc/client";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { CheckoutItem } from "../components/checkout-item";
+import { generateTenantURL } from "@/lib/utils";
 interface CheckoutViewProps {
     tenantSlug: string;
 }
 
 export const CheckoutView = ({tenantSlug} : CheckoutViewProps) => {
-    const {productIds, clearAllCarts} = useCart(tenantSlug);
+    const {productIds, clearAllCarts, removeProduct} = useCart(tenantSlug);
     
     const trpc = useTRPC()
     const {data, isLoading, error} = useQuery(trpc.checkout.getProducts.queryOptions({
@@ -25,8 +27,29 @@ export const CheckoutView = ({tenantSlug} : CheckoutViewProps) => {
     }, [error])
    
     return (
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+            <div className="grid grid-cols-1 lg:grid-cols-7 gap4 lg:gap-16">
+                <div className="lg:col-span-4"> 
+                    <div className="border rounded-md overflow-hidden bg-white">
+                        {data?.docs.map((product) => (
+                            <div key={product.id}>
+                                <CheckoutItem
+                                    imageUrl={product.image?.url}
+                                    name={product.name}
+                                    productUrl={`${generateTenantURL(product.tenant.slug)}/products/${product.id}`}
+                                    tenantUrl={generateTenantURL(product.tenant.slug)}
+                                    tenantName={product.tenant.name}
+                                    price={product.price}
+                                    onRemove={() => removeProduct(product.id)}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="lg:col-span-3">
+                    Checkout sidebar
+                </div>
+            </div>
         </div>
     )
 }
