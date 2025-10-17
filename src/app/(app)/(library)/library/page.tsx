e00990a664eb4a8abca3a7091cde1530ed9faa9a
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-10-17 16:25:23 +0200

    feat: added a library for frontend

---
+++ src/app/(app)/(library)/library/page.tsx
@@ -0,0 +1,20 @@
+import { DEFAULT_PAGE_LIMIT } from "@/constants";
+import { LibraryView } from "@/modules/library/ui/views/library-view"
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+const Page = () => {
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchInfiniteQuery(trpc.library.getMany.infiniteQueryOptions({
+        limit: DEFAULT_PAGE_LIMIT,
+    }))
+    
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <LibraryView/>
+        </HydrationBoundary>
+    )
+}
+
+export default Page
