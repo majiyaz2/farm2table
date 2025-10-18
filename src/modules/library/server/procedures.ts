7f53c6c78d963a16e7402ef1a1f86d4844e84de4
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-10-18 22:21:47 +0200

    feat: added a procedure to get a single order

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -3,9 +3,60 @@ import { Media, Tenant } from "@/payload-types";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { z } from "zod";
 import { DEFAULT_PAGE_LIMIT } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const libraryRouter = createTRPCRouter({
+    getOne: protectedProcedure
+    .input(
+        z.object({
+        productId: z.string(),
+    })
+    )
+    .query(async ({ctx, input}) => {
+        const orderData = await ctx.db.find({
+            collection: "orders",
+            limit: 1,
+            pagination: false,
+            where: {
+                and: [
+                    {
+                        user: {
+                            equals: ctx.session.user.id
+                        }
+                    },
+                    {
+                        products: {
+                            equals: input.productId
+                        }
+                    }
+                ]
+            }
+        });
 
+        const order = orderData.docs[0];
+        
+        if(!order){
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Order not found"
+            })
+        }
+
+        const product = await ctx.db.findByID({
+            collection: "products",
+            id: input.productId
+        })
+
+        if(!product){
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "Product not found"
+            })
+        }
+
+
+        return product;
+    }),
     getMany: protectedProcedure.input(z.object({
         cursor: z.number().default(1),
         limit: z.number().default(DEFAULT_PAGE_LIMIT),
