7ee24c2d95cf5913cd3124bc5ca17a637651590d
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-10-18 23:07:48 +0200

    feat: added collections for reviews

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -15,6 +15,7 @@ import { Products } from './collections/Products'
 import { Tags } from './collections/Tags'
 import { Orders } from './collections/Orders'
 import { Tenants } from './collections/Tenants'
+import { Reviews } from './collections/Reviews'
 
 
 const filename = fileURLToPath(import.meta.url)
@@ -27,7 +28,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories, Products, Tags, Tenants, Orders],
+  collections: [Users, Media, Categories, Products, Tags, Tenants, Orders, Reviews],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
